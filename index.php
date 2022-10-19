ec41695a3b0bd2710a49acf2fc2e8a2b997721d2
Nwpatt <nawapat_link@hotmail.com>
2022-10-19 12:56:54 +0700

    webboard.nwpatt w/ boostrap

--- index.php
+++ index.php
@@ -8,6 +8,11 @@ session_start();
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"/></link>
   <title>Webboard's Nwpatt</title>
 </head>
 
@@ -16,23 +21,40 @@ if (!isset($_SESSION["id"])) {
 ?>
 
   <body>
+    <div class="container">
     <h1 style="text-align: center"> Webboard's Nwpatt</h1>
-    <hr />
+    <?php include "nav.php";?>
+    <!-- <hr />
     หมวดหมู่ :
     <select name="category">
       <option value="all">--ทั้งหมด--</option>
       <option value="genaral">เรื่องทั่วไป</option>
       <option value="stady">เรื่องเรียน</option>
     </select>
-    <a href="login.php" style="float: right"> เข้าสู่ระบบ </a>
-    <br />
-    <ul>
+    <a href="login.php" style="float: right"> เข้าสู่ระบบ </a> -->
+    <br>
+    <div class="d-flex">
+      <div>
+        <label>หมวดหมู่</label>
+        <span class="dropdown">
+          <button class="btn btn-light dropdown-toggle btn-sm" type="button" id="button2" data-bs-toggle="dropdown" aria-expanded="false">--ทั้งหมด--</button>
+          <ul class="dropdown-menu" aria-labelledby="button2">
+            <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+            <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+            <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+          </ul>
+        </span>
+      </div>
+    </div>
+    <br/>
+    <table class="table table-striped">
       <?php
       for ($i = 1; $i <= 10; $i++) {
-        echo "<li><a href=post.php?id=" . "$i" . ">กระทู้ที่ " . $i . "</a></li>";
+        echo "<tr><td><a href=post.php?id=" . "$i" . " style=text-decoration:none>กระทู้ที่ " . $i . "</a></td></tr>";
       }
       ?>
-    </ul>
+    </table>
+    </div>
   </body>
 
 <?php
@@ -40,7 +62,9 @@ if (!isset($_SESSION["id"])) {
 ?>
 
   <body>
+    <div class="container">
     <h1 style="text-align: center"> Webboard's Nwpatt</h1>
+    <?php include "nav.php";?>
     <hr />
     หมวดหมู่ :
     <select name="category">
@@ -51,7 +75,8 @@ if (!isset($_SESSION["id"])) {
     <div style="float: right;">
     <span>ผู้ใช้งานระบบ : <?php echo $_SESSION["username"]?></span> &nbsp;
     <a href="logout.php" style="float: right"> ออกจากระบบ </a>
-    </div><br> 
+    </div>
+    <br> 
     <a href="newpost.php">สร้างกระทู้ใหม่</a>
     <br/>
     <ul>
@@ -65,6 +90,7 @@ if (!isset($_SESSION["id"])) {
       }
       ?>
     </ul>
+    </div>
   </body>
 <?php
 }
