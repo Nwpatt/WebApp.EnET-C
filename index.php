e780b046dddab604ec4d0b49af56a8e44389644f
Nwpatt <nawapat_link@hotmail.com>
2022-10-26 10:35:48 +0700

    'final'

--- index.php
+++ index.php
@@ -10,9 +10,8 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-  <link
-      rel="stylesheet"
-      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"/></link>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css" />
+  </link>
   <title>Webboard's Nwpatt</title>
 </head>
 
@@ -22,9 +21,9 @@ if (!isset($_SESSION["id"])) {
 
   <body>
     <div class="container">
-    <h1 style="text-align: center"> Webboard's Nwpatt</h1>
-    <?php include "nav.php";?>
-    <!-- <hr />
+      <h1 style="text-align: center"> Webboard's Nwpatt</h1>
+      <?php include "nav.php"; ?>
+      <!-- <hr />
     หมวดหมู่ :
     <select name="category">
       <option value="all">--ทั้งหมด--</option>
@@ -32,28 +31,29 @@ if (!isset($_SESSION["id"])) {
       <option value="stady">เรื่องเรียน</option>
     </select>
     <a href="login.php" style="float: right"> เข้าสู่ระบบ </a> -->
-    <br>
-    <div class="d-flex">
-      <div>
-        <label>หมวดหมู่</label>
-        <span class="dropdown">
-          <button class="btn btn-light dropdown-toggle btn-sm" type="button" id="button2" data-bs-toggle="dropdown" aria-expanded="false">--ทั้งหมด--</button>
-          <ul class="dropdown-menu" aria-labelledby="button2">
-            <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
-            <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
-            <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
-          </ul>
-        </span>
+      <br>
+      <div class="d-flex justify-content-between">
+        <div>
+          <label>หมวดหมู่</label>
+          <span class="dropdown">
+            <button class="btn btn-light dropdown-toggle btn-sm" type="button" id="button2" data-bs-toggle="dropdown" aria-expanded="false">--ทั้งหมด--</button>
+            <ul class="dropdown-menu" aria-labelledby="button2">
+              <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+              <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+              <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+            </ul>
+          </span>
+        </div>
+        <div><a href="newpost.php" class="btn btn-success btn-sm"><i class="bi bi-bookmark-plus pe-2 "></i>สร้างกระทู้ใหม่</a></div>
       </div>
-    </div>
-    <br/>
-    <table class="table table-striped">
-      <?php
-      for ($i = 1; $i <= 10; $i++) {
+      <br />
+      <table class="table table-striped">
+        <?php
+       for ($i = 1; $i <= 10; $i++) {
         echo "<tr><td><a href=post.php?id=" . "$i" . " style=text-decoration:none>กระทู้ที่ " . $i . "</a></td></tr>";
-      }
-      ?>
-    </table>
+       }
+        ?>
+      </table>
     </div>
   </body>
 
@@ -63,35 +63,37 @@ if (!isset($_SESSION["id"])) {
 
   <body>
     <div class="container">
-    <h1 style="text-align: center"> Webboard's Nwpatt</h1>
-    <?php include "nav.php";?>
-    <hr />
-    หมวดหมู่ :
-    <select name="category">
-      <option value="all">--ทั้งหมด--</option>
-      <option value="genaral">เรื่องทั่วไป</option>
-      <option value="stady">เรื่องเรียน</option>
-    </select>
-    <div style="float: right;">
-    <span>ผู้ใช้งานระบบ : <?php echo $_SESSION["username"]?></span> &nbsp;
-    <a href="logout.php" style="float: right"> ออกจากระบบ </a>
-    </div>
-    <br> 
-    <a href="newpost.php">สร้างกระทู้ใหม่</a>
-    <br/>
-    <ul>
-      <?php
-      for ($i = 1; $i <= 10; $i++) {
-        echo "<li><a href=post.php?id=" . "$i" . ">กระทู้ที่ " . $i . "</a>";
-        if($_SESSION["role"]=="a"){
-          echo "&emsp; <a href=delete.php?id=$i>ลบ</a>";
-        }
-        echo "</li>";
-      }
-      ?>
-    </ul>
+      <h1 style="text-align: center"> Webboard's Nwpatt</h1>
+      <?php include "nav.php"; ?>
+      <br>
+      <div class="d-flex">
+        <div>
+          <label>หมวดหมู่</label>
+          <span class="dropdown">
+            <button class="btn btn-light dropdown-toggle btn-sm" type="button" id="button2" data-bs-toggle="dropdown" aria-expanded="false">--ทั้งหมด--</button>
+            <ul class="dropdown-menu" aria-labelledby="button2">
+              <li><a href="#" class="dropdown-item">ทั้งหมด</a></li>
+              <li><a href="#" class="dropdown-item">เรื่องเรียน</a></li>
+              <li><a href="#" class="dropdown-item">เรื่องทั่วไป</a></li>
+            </ul>
+          </span>
+        </div>
+      </div>
+      <br />
+      <table class="table table-striped">
+        <?php
+        for ($i = 1; $i <= 10; $i++) {
+          echo "<tr><td><a href=post.php?id=" . "$i" . " style=text-decoration:none>กระทู้ที่ " . $i . "</a></td>";
+          if($_SESSION["role"]=="a"){
+            echo "<td><a href=delete.php?id=$i class='btn btn-danger btn-sm'><i class='bi bi-trash'></i></a></td>";
+          }
+          echo "</tr>";
+        }   
+        ?>
+      </table>
     </div>
   </body>
+
 <?php
 }
 ?>
--- verify.php
+++ verify.php
@@ -5,43 +5,28 @@
       die();
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<?php
+    $login = $_POST["login"];
+    $pwd = $_POST["pwd"];
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Verify</title>
-</head>
-
-<body>
-
-    <h1 style="text-align: center">Webboard's Nwpatt</h1>
-    <hr>
-    <center>
-        <?php
-        $login = $_POST["login"];
-        $pwd = $_POST["pwd"];
-
-        if (($login == "admin") && ( $pwd == "ad1234")){
-            echo "<p>ยินดีต้อนรับคุณ ADMIN</p>";
+    if (($login == "admin") && ( $pwd == "ad1234")){
             $_SESSION["username"]="admin";
             $_SESSION["role"]="a";
-            $_SESSION["id"]="session_id()";}
-
+            $_SESSION["id"]="session_id()";
+            header("location:index.php");
+            die();
+        }
         elseif (($login == "member") && ($pwd == "mem1234")){
-            echo "<p>ยินดีต้อนรับคุณ MEMBER</p>";
             $_SESSION["username"]="member";
             $_SESSION["role"]="m";
-            $_SESSION["id"]="session_id()";}
+            $_SESSION["id"]="session_id()";
+            header("location:index.php");
+            die();
+        }
             
         else{
-            echo "<p>ชื่อบัญชีหรือรหัสผ่านไม่ถูกต้อง</p>";}
-        ?>
-        <a href="index.php">เข้าสู่หน้าหลัก</a>
-    </center>
-
-</body>
-
-</html>
+            $_SESSION['error']='error';
+            header("location:login.php");
+            die();
+        }
+?>
